cd0ac20e551b987e0133e5040409e3a1c47fb07f
vijaysing514 <vijaysing514@gmail.com>
2023-12-26 00:43:33 +0530

    adding content about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,4 +1,4 @@
-import React , {useState} from "react";
+// import React , {useState} from "react";
 import ParticlesContainer from '../../components/ParticlesContainer'
 import Avatar from "../../components/Avatar";
 import Circles from "../../components/Circles"
@@ -7,97 +7,97 @@ import { fadeIn } from "../../variants";
 import CountUp from "react-countup";
 
 // icons
-import {
-  FaHtml5,
-  FaCss3,
-  FaJs,
-  FaReact,
-  FaWordpress,
-  FaFigma,
-} from "react-icons/fa";
+// import {
+//   FaHtml5,
+//   FaCss3,
+//   FaJs,
+//   FaReact,
+//   FaWordpress,
+//   FaFigma,
+// } from "react-icons/fa";
 
-import {
-  SiNextdotjs,
-  SiFramer,
-  SiAdobexd,
-  SiAdobephotoshop,
-} from "react-icons/si";
+// import {
+//   SiNextdotjs,
+//   SiFramer,
+//   SiAdobexd,
+//   SiAdobephotoshop,
+// } from "react-icons/si";
 
 //  data
-const aboutData = [
-  {
-    title: 'skills',
-    info: [
-      {
-        title: 'Web Development',
-        icons: [
-          <FaHtml5 />,
-          <FaCss3 />,
-          <FaJs />,
-          <FaReact />,
-          <SiNextdotjs />,
-          <SiFramer />,
-          <FaWordpress />,
-        ],
-      },
-      {
-        title: 'UI/UX Design',
-        icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
-      },
-    ],
-  },
-  {
-    title: 'awards',
-    info: [
-      {
-        title: 'Webby Awards - Honoree',
-        stage: '2011 - 2012',
-      },
-      {
-        title: 'Adobe Design Achievement Awards - Finalist',
-        stage: '2009 - 2010',
-      },
-    ],
-  },
-  {
-    title: 'experience',
-    info: [
-      {
-        title: 'UX/UI Designer - XYZ Company',
-        stage: '2012 - 2023',
-      },
-      {
-        title: 'Web Developer - ABC Agency',
-        stage: '2010 - 2012',
-      },
-      {
-        title: 'Intern - DEF Corporation',
-        stage: '2008 - 2010',
-      },
-    ],
-  },
-  {
-    title: 'credentials',
-    info: [
-      {
-        title: 'Web Development - ABC University, LA, CA',
-        stage: '2011',
-      },
-      {
-        title: 'Computer Science Diploma - AV Technical Institute',
-        stage: '2009',
-      },
-      {
-        title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
-        stage: '2006',
-      },
-    ],
-  },
-];
+// const aboutData = [
+//   {
+//     title: 'skills',
+//     info: [
+//       {
+//         title: 'Web Development',
+//         icons: [
+//           <FaHtml5 />,
+//           <FaCss3 />,
+//           <FaJs />,
+//           <FaReact />,
+//           <SiNextdotjs />,
+//           <SiFramer />,
+//           <FaWordpress />,
+//         ],
+//       },
+//       {
+//         title: 'UI/UX Design',
+//         icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
+//       },
+//     ],
+//   },
+//   {
+//     title: 'awards',
+//     info: [
+//       {
+//         title: 'Webby Awards - Honoree',
+//         stage: '2011 - 2012',
+//       },
+//       {
+//         title: 'Adobe Design Achievement Awards - Finalist',
+//         stage: '2009 - 2010',
+//       },
+//     ],
+//   },
+//   {
+//     title: 'experience',
+//     info: [
+//       {
+//         title: 'UX/UI Designer - XYZ Company',
+//         stage: '2012 - 2023',
+//       },
+//       {
+//         title: 'Web Developer - ABC Agency',
+//         stage: '2010 - 2012',
+//       },
+//       {
+//         title: 'Intern - DEF Corporation',
+//         stage: '2008 - 2010',
+//       },
+//     ],
+//   },
+//   {
+//     title: 'credentials',
+//     info: [
+//       {
+//         title: 'Web Development - ABC University, LA, CA',
+//         stage: '2011',
+//       },
+//       {
+//         title: 'Computer Science Diploma - AV Technical Institute',
+//         stage: '2009',
+//       },
+//       {
+//         title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
+//         stage: '2006',
+//       },
+//     ],
+//   },
+// ];
 
 const About = () => {
 
-  const [index,setIndex] = useState(0);
+  // const [index,setIndex] = useState(0);
 
   return <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
     <ParticlesContainer />
