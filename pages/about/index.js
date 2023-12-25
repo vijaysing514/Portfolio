3b31782acb3313a3a5ff234909248fe1772e5053
vijaysing514 <vijaysing514@gmail.com>
2023-12-26 00:14:02 +0530

    fixing bugs at deployment

--- pages/about/index.js
+++ pages/about/index.js
@@ -149,8 +149,8 @@ const About = () => {
               <div className="hidden md:flex">-</div>
               <div>{item.stage}</div>
               <div className="flex gap-x-4">
-              {item.icons?.map((icon,itemIndex)=>{
-                return <div key={itemIndex} className="text-2xl text-white">{icon}</div>
+              {item.icons?.map((icon,iconIndex)=>{
+                return <div key={iconIndex} className="text-2xl text-white">{icon}</div>
               })}
               </div>
             </div>
