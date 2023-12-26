dd36af2fc4a266d1ebac32078da27a04a614292e
vijaysing514 <vijaysing514@gmail.com>
2023-12-27 01:01:47 +0530

    adding content in about us page

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,4 +1,4 @@
-// import React , {useState} from "react";
+import React , {useState} from "react";
 import ParticlesContainer from '../../components/ParticlesContainer'
 import Avatar from "../../components/Avatar";
 import Circles from "../../components/Circles"
@@ -6,98 +6,98 @@ import { motion } from "framer-motion";
 import { fadeIn } from "../../variants";
 import CountUp from "react-countup";
 
-// icons
-// import {
-//   FaHtml5,
-//   FaCss3,
-//   FaJs,
-//   FaReact,
-//   FaWordpress,
-//   FaFigma,
-// } from "react-icons/fa";
+//icons
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaWordpress,
+  FaFigma,
+} from "react-icons/fa";
 
-// import {
-//   SiNextdotjs,
-//   SiFramer,
-//   SiAdobexd,
-//   SiAdobephotoshop,
-// } from "react-icons/si";
+import {
+  SiNextdotjs,
+  SiFramer,
+  SiAdobexd,
+  SiAdobephotoshop,
+} from "react-icons/si";
 
-//  data
-// const aboutData = [
-//   {
-//     title: 'skills',
-//     info: [
-//       {
-//         title: 'Web Development',
-//         icons: [
-//           <FaHtml5 />,
-//           <FaCss3 />,
-//           <FaJs />,
-//           <FaReact />,
-//           <SiNextdotjs />,
-//           <SiFramer />,
-//           <FaWordpress />,
-//         ],
-//       },
-//       {
-//         title: 'UI/UX Design',
-//         icons: [<FaFigma />, <SiAdobexd />, <SiAdobephotoshop />],
-//       },
-//     ],
-//   },
-//   {
-//     title: 'awards',
-//     info: [
-//       {
-//         title: 'Webby Awards - Honoree',
-//         stage: '2011 - 2012',
-//       },
-//       {
-//         title: 'Adobe Design Achievement Awards - Finalist',
-//         stage: '2009 - 2010',
-//       },
-//     ],
-//   },
-//   {
-//     title: 'experience',
-//     info: [
-//       {
-//         title: 'UX/UI Designer - XYZ Company',
-//         stage: '2012 - 2023',
-//       },
-//       {
-//         title: 'Web Developer - ABC Agency',
-//         stage: '2010 - 2012',
-//       },
-//       {
-//         title: 'Intern - DEF Corporation',
-//         stage: '2008 - 2010',
-//       },
-//     ],
-//   },
-//   {
-//     title: 'credentials',
-//     info: [
-//       {
-//         title: 'Web Development - ABC University, LA, CA',
-//         stage: '2011',
-//       },
-//       {
-//         title: 'Computer Science Diploma - AV Technical Institute',
-//         stage: '2009',
-//       },
-//       {
-//         title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
-//         stage: '2006',
-//       },
-//     ],
-//   },
-// ];
+ //data
+const aboutData = [
+  {
+    title: 'skills',
+    info: [
+      {
+        title: 'Web Development',
+        icons: [
+          <FaHtml5 key="html5"/>,
+          <FaCss3 key="Css3"/>,
+          <FaJs key="FaJs"/>,
+          <FaReact key="FaReact"/>,
+          <SiNextdotjs key="SiNextdotjs"/>,
+          <SiFramer key="SiFramer"/>,
+          <FaWordpress key="FaWordpress"/>,
+        ],
+      },
+      {
+        title: 'UI/UX Design',
+        icons: [<FaFigma key="FaFigma"/>, <SiAdobexd key="SiAdobexd" />, <SiAdobephotoshop key="SiAdobephotoshop"/>],
+      },
+    ],
+  },
+  {
+    title: 'awards',
+    info: [
+      {
+        title: 'Webby Awards - Honoree',
+        stage: '2011 - 2012',
+      },
+      {
+        title: 'Adobe Design Achievement Awards - Finalist',
+        stage: '2009 - 2010',
+      },
+    ],
+  },
+  {
+    title: 'experience',
+    info: [
+      {
+        title: 'UX/UI Designer - XYZ Company',
+        stage: '2012 - 2023',
+      },
+      {
+        title: 'Web Developer - ABC Agency',
+        stage: '2010 - 2012',
+      },
+      {
+        title: 'Intern - DEF Corporation',
+        stage: '2008 - 2010',
+      },
+    ],
+  },
+  {
+    title: 'credentials',
+    info: [
+      {
+        title: 'Web Development - ABC University, LA, CA',
+        stage: '2011',
+      },
+      {
+        title: 'Computer Science Diploma - AV Technical Institute',
+        stage: '2009',
+      },
+      {
+        title: 'Certified Graphic Designer - ABC Institute, Los Angeles, CA',
+        stage: '2006',
+      },
+    ],
+  },
+];
 
 const About = () => {
 
-  // const [index,setIndex] = useState(0);
+  const [index,setIndex] = useState(0);
 
   return <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
     <ParticlesContainer />
@@ -127,7 +127,7 @@ Greetings! I&apos;m Vijay, and I derive immense joy from crafting digital entiti
           </div>
         </motion.div>
       </div>
-      {/* <motion.div variants={fadeIn('left',0.6)} initial="hidden" animate="show" exit="hidden" className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
+      <motion.div variants={fadeIn('left',0.6)} initial="hidden" animate="show" exit="hidden" className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
         <div className="flex mx-auto gap-x-4 xl:gap-x-8 xl:mx-0 md-4">
           {aboutData.map((item,itemIndex)=>{
             return(
@@ -138,8 +138,8 @@ Greetings! I&apos;m Vijay, and I derive immense joy from crafting digital entiti
               </div>
             )
           })}
-        </div> */}
-        {/* <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start my-2">
+        </div>
+        <div className="py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start my-2">
           {aboutData[index].info.map((item,itemIndex)=>{
             return <div key={item.id} className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60">
               <div className="font-light mb-2 md:mb-0">{item.title}</div>
@@ -152,8 +152,8 @@ Greetings! I&apos;m Vijay, and I derive immense joy from crafting digital entiti
               </div>
             </div>
           })}
-        </div> */}
-      {/* </motion.div> */}
+        </div>
+      </motion.div>
     </div>
   </div>;
 };
