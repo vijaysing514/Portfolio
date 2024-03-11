e7b014e2790c6597b6359e3367edb4cc129d3ba9
vijaysing514 <93137994+vijaysing514@users.noreply.github.com>
2024-03-11 19:15:16 +0530

    Update index.js

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -1,1 +1,30 @@
 
+import ParticlesContainer from '../../components/ParticlesContainer';
+import ServiceSlider from '../../components/ServiceSlider';
+import Bulb from '../../components/Bulb';
+import Circles from '../../components/Circles';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
+
+
+
+const Resume = () => {
+  return <div className='bg-primary/30 h-full py-36 flex items-center'>
+    <ParticlesContainer />
+    <Circles />
+    <div className='container mx-auto'>
+      <div className='flex flex-col xl:flex-row gap-x-8'>
+        <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
+          <motion.h2 variants={fadeIn('up',0.3)} initial="hidden" animate="show" exit="hidden" className='h2 xl:mt-8'>My services <span className='text-accent'>.</span></motion.h2>
+          <motion.p variants={fadeIn('up',0.4)} initial="hidden" animate="show" exit="hidden" className='mb-4 max-w-[400px] mx-auto lg:mx-0'>Embark on a journey of digital transformation with my comprehensive range of services tailored to elevate your online presence.</motion.p>
+        </div>
+        <motion.div variants={fadeIn('up',0.6)} initial="hidden" animate="show" exit="hidden" className='w-full xl:max-w-[65%]'>
+        <ServiceSlider />
+        </motion.div>
+      </div>
+    </div>
+    <Bulb />
+  </div>;
+};
+
+export default Resume;
