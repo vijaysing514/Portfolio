5649411092e2ae4c254e1e3626c708c0c1c9edfc
vijaysing514 <93137994+vijaysing514@users.noreply.github.com>
2024-03-11 19:23:42 +0530

    Update index.js

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -9,8 +9,6 @@ import { fadeIn } from '../../variants';
 
 const Resume = () => {
   return <div className='bg-primary/30 h-full'>
-    <ParticlesContainer />
-    <Circles />
     <div className='container mx-auto'>
       <div className=''>
         <motion.div variants={fadeIn('up',0.6)} initial="hidden" animate="show" exit="hidden" className='w-full xl:max-w-[65%]'>
