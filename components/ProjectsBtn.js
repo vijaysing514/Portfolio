1afe4efe96157fbeafbfe5b34b24231c0098ea69
vijaysing514 <93137994+vijaysing514@users.noreply.github.com>
2024-03-11 19:49:05 +0530

    Update ProjectsBtn.js

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -3,11 +3,28 @@ import Link from "next/link";
 import {HiArrowRight} from 'react-icons/hi2'
 
 const ProjectsBtn = () => {
+
+  const handleDownloadResume = () => {
+  // Replace the fileUrl with the actual URL of your resume PDF
+  const fileUrl = '/vijay-singh.pdf';
+  window.open(fileUrl, '_blank');
+};
+  
   return <div className="mx-auto xl:mx-0 ">
-    <Link href={'/work'} className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-no-repeat group">
-      <Image src={'/rounded-text.png'} width={141} height={148} alt="" className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"/>
-      <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300"/>
-    </Link>
+    <a
+    href="#"
+    onClick={handleDownloadResume}
+    className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-no-repeat group"
+  >
+    <Image
+      src="/rounded-text.png"
+      width={141}
+      height={148}
+      alt=""
+      className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"
+    />
+    <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300" />
+  </a>
   </div>;
 };
 
