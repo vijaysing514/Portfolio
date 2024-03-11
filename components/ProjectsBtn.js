cd06b56b4bc438f38435db881a6bc3a075ee33a9
vijaysing514 <93137994+vijaysing514@users.noreply.github.com>
2024-03-11 19:56:03 +0530

    Update ProjectsBtn.js

--- components/ProjectsBtn.js
+++ components/ProjectsBtn.js
@@ -3,35 +3,12 @@ import Link from "next/link";
 import {HiArrowRight} from 'react-icons/hi2'
 
 const ProjectsBtn = () => {
-
-  const handleDownloadResume = () => {
-  // Replace the fileUrl with the actual URL of your resume PDF
-  const fileUrl = '/vijay-singh.pdf';
-  // window.open(fileUrl, '_blank');
-    // Create a temporary anchor element
-  const link = document.createElement('a');
-  link.href = fileUrl;
-  link.setAttribute('download', 'resume.pdf'); // Set the filename for the downloaded file
-  document.body.appendChild(link);
-  link.click(); // Simulate a click on the anchor element
-  document.body.removeChild(link); // Clean up: remove the anchor element from the document
-};
   
   return <div className="mx-auto xl:mx-0 ">
-    <a
-    href="#"
-    onClick={handleDownloadResume}
-    className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-no-repeat group"
-  >
-    <Image
-      src="/rounded-text.png"
-      width={141}
-      height={148}
-      alt=""
-      className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"
-    />
-    <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300" />
-  </a>
+    <Link href={'/work'} className="relative w-[185px] h-[185px] flex justify-center items-center bg-circleStar bg-cover bg-no-repeat group">
+      <Image src={'/rounded-text.png'} width={141} height={148} alt="" className="animate-spin-slow w-full h-full max-w-[141px] max-h-[148px]"/>
+      <HiArrowRight className="absolute text-4xl group-hover:translate-x-2 transition-all duration-300"/>
+    </Link>
   </div>;
 };
 
