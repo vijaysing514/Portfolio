8a3b2af0415c82f6558a53c57de4cf549522ca34
vijaysing514 <93137994+vijaysing514@users.noreply.github.com>
2024-03-11 19:19:12 +0530

    Update index.js

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -13,10 +13,6 @@ const Resume = () => {
     <Circles />
     <div className='container mx-auto'>
       <div className='flex flex-col xl:flex-row gap-x-8'>
-        <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
-          <motion.h2 variants={fadeIn('up',0.3)} initial="hidden" animate="show" exit="hidden" className='h2 xl:mt-8'>My services <span className='text-accent'>.</span></motion.h2>
-          <motion.p variants={fadeIn('up',0.4)} initial="hidden" animate="show" exit="hidden" className='mb-4 max-w-[400px] mx-auto lg:mx-0'>Embark on a journey of digital transformation with my comprehensive range of services tailored to elevate your online presence.</motion.p>
-        </div>
         <motion.div variants={fadeIn('up',0.6)} initial="hidden" animate="show" exit="hidden" className='w-full xl:max-w-[65%]'>
         <div>
       <h1>PDF Viewer</h1>
