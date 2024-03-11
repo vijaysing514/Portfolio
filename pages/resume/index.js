ac66ed69e776628570aee0cbd81a3a8a5e2d3c6e
vijaysing514 <93137994+vijaysing514@users.noreply.github.com>
2024-03-11 19:32:50 +0530

    Update index.js

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -8,18 +8,19 @@ import { fadeIn } from '../../variants';
 
 
 const Resume = () => {
+   const handleDownload = () => {
+    // Assuming fileUrl is the URL of the file you want to download
+    const fileUrl = '/vijay-singh.pdf';
+    
+    // Triggering the download
+    window.open(fileUrl, '_blank');
+  };
    return <div className='bg-primary/30 h-full py-36 flex items-center'>
     <ParticlesContainer />
     <Circles />
     <div>
-      <h1>PDF Viewer</h1>
-      <iframe
-        title="PDF Viewer"
-        src="vijay-singh.pdf"
-        width="100%"
-        height="500px"
-        frameborder="0"
-      ></iframe>
+      <h1>Download Resume</h1>
+      <button onClick={handleDownload}>Download Resume</button>
     </div>
     <Bulb />
   </div>;
