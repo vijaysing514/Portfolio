420ae4e4af87722835a0d9ff32352bb0c39a5081
vijaysing514 <93137994+vijaysing514@users.noreply.github.com>
2024-03-20 20:39:42 +0530

    Update WorkSlider.js

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -20,18 +20,22 @@ const workSlides = [
             {
               title: 'https://dashboard-vijay.vercel.app/',
               path: '/dashboard-app.png',
+              name: 'Dashboard App',
             },
             {
               title: 'https://vijay-chatting.vercel.app/',
               path: '/chatapp.png',
+              name: 'Chat Bot App',
             },
             {
               title: 'https://vijay-cricket-score-backend.vercel.app/',
               path: '/Cricket-Project-score-backend.png',
+              name: 'Cricket Sorecard App',
             },
              {
               title: 'https://vijay-image-ai.vercel.app/',
               path: '/imageai.png',
+               name: 'AI Image Genration App',
             },
           ],
         },
@@ -40,10 +44,12 @@ const workSlides = [
             {
               title: 'https://vinstagram.vercel.app/',
               path: '/vinstagram.png',
+              name: 'Instagram Clone',
             },
             {
               title: 'https://vijaysingh.vercel.app/',
               path: '/portfolio.png',
+              name: 'My Portfolio',
             },
           ],
         },
@@ -57,18 +63,22 @@ const workSlides = [
           {
             title: 'https://b2bgrowthexpo.com/',
             path: '/b2bexpo.png',
+            name: 'Live Website',
           },
           {
             title: 'https://marshamcourthotel.co.uk/',
             path: '/marshamcourt.png',
+            name: 'Live Website',
           },
           {
             title: 'https://www.sirmaxochem.in/',
             path: '/sirmaxo.png',
+            name: 'Live Website',
           },
           {
             title: 'https://unity101.org/',
             path: '/unity.png',
+            name: 'Live Website',
           },
         ],
       },
@@ -77,18 +87,22 @@ const workSlides = [
           {
             title: 'http://customtheme.singhvijay.in/?i=1',
             path: '/customt.png',
+            name: 'Live Website',
           },
           {
             title: 'https://bbxuk.com/',
             path: '/bbx.png',
+            name: 'Live Website',
           },
           {
             title: 'https://www.visualytes.com/',
             path: '/visu.png',
+            name: 'Live Website',
           },
           {
             title: 'https://tideandseektravel.com/',
             path: '/tide.png',
+            name: 'Live Website',
           },
         ],
       },
@@ -157,11 +171,11 @@ const WorkSlider = () => {
                 <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-300'></div>
                 <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300'>
                 <div className='flex items-center gap-x-2 text-[13px] tracking-[0.2em]'>
-                <Link target="_blank" href={image.title}><div className='delay-100'>LIVE</div></Link>
+                // <Link target="_blank" href={image.title}><div className='delay-100'>LIVE</div></Link>
                 <Link target="_blank" href={image.title}>
-                  <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>PROJECT</div>
+                  <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-100'>{image.name}</div>
                 </Link>
-                <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200 text-xl'>
+                <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150 text-xl'>
                 <Link target="_blank" href={image.title}><BsArrowRight /></Link>
                 </div>
                 </div>
